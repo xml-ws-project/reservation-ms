e7d9cac8d17998fcd338113f097d1693802415b0
Vuk Milanovic <vuk.milanovic11@gmial.com>
2023-05-14 01:31:40 +0200

    search feature implemented

--- src/main/java/com/vima/reservation/repository/ReservationRepository.java
+++ src/main/java/com/vima/reservation/repository/ReservationRepository.java
@@ -22,9 +22,7 @@ public interface ReservationRepository extends MongoRepository<Reservation, UUID
         " 'accomInfo.city' : { $regex : /^?2/, $options: 'i' }," +
         " 'accomInfo.minGuests' : { $lte : ?3 }," +
         " 'accomInfo.maxGuests' : { $gte : ?3 }," +
-        " $or : [ {$and: [{'desiredDate.start' : { $lte : ?4 }}, {'desiredDate.end' : { $gte : ?4 }}] }," +
-        " {$and: [{'desiredDate.start' : { $lte : ?5 }}, {'desiredDate.end' : { $gte : ?5 }}] } ] }")
+        " $or : [ {$and: [{'desiredDate.start' : { $lt : ?4 }}, {'desiredDate.end' : { $gt : ?4 }}] }," +
+        " {$and: [{'desiredDate.start' : { $lt : ?5 }}, {'desiredDate.end' : { $gt : ?5 }}] } ] }")
     List<Reservation> searchReservations(ReservationStatus status, String country, String city, int guests, LocalDate periodStart, LocalDate periodEnd);
 }
-
-//$or : [{ 'desiredDate.start' : { $gt : '?4', $lt : '?5' } }, { 'desiredDate.end' : { $gt : '?4', $lt : '?5' } }]
